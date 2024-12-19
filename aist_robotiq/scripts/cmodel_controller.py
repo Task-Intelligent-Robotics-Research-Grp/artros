8297f1285ff2a1cdf2315087908a959d5180efc5
Toshio UESHIBA <t.ueshiba@aist.go.jp>
2024-12-19 17:42:29 +0900

    Make more strict the condition for grippers reaching goal

--- aist_robotiq/scripts/cmodel_controller.py
+++ aist_robotiq/scripts/cmodel_controller.py
@@ -223,7 +223,8 @@ class CModelController(object):
 
     def _reached_goal(self, status):
         # ibid
-        return abs(status.gPO - self._goal_rPR) <= 1
+        # return abs(status.gPO - self._goal_rPR) <= 1
+        return status.gPO == self._goal_rPR
 
     def _status_values(self, status):
         return self._position(status), self._effort(status), \
