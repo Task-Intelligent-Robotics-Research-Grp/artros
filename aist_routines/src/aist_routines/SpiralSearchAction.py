4d80db53bed3431e13cecfa1adb96da2109cd178
t-ueshiba <t.ueshiba@aist.go.jp>
2025-05-24 06:21:28 +0900

    Allow elliptic trajectory for SpiralSearch

--- aist_routines/src/aist_routines/SpiralSearchAction.py
+++ aist_routines/src/aist_routines/SpiralSearchAction.py
@@ -49,7 +49,8 @@ class SpiralSearch(SimpleActionClient):
         SimpleActionClient.__init__(self, 'spiral_search', SpiralSearchAction)
 
         self._routines = routines
-        self._server   = SimpleActionServer("spiral_search",
+        self._success  = False
+        self._server   = SimpleActionServer('spiral_search',
                                             SpiralSearchAction,
                                             self._execute_cb, False)
         self._server.register_preempt_callback(self._preempt_cb)
@@ -58,52 +59,69 @@ class SpiralSearch(SimpleActionClient):
 
     # Client stuffs
     def send_goal(self, robot_name, eef_link='',
-                  angle_increment=30.0, radius_increment=0.00005,
-                  radius_max=0.002, speed=0.005, accel=1.0,
-                  timeout=rospy.Duration(60.0), done_cb=None, active_cb=None):
+                  npoints=36, angle_increment=30.0,
+                  radius_y_max=0.002, radius_x_max=0.005,
+                  speed=0.005, accel=1.0, timeout=rospy.Duration(60.0),
+                  done_cb=None, active_cb=None):
         SimpleActionClient.send_goal(self,
                                      SpiralSearchGoal(robot_name, eef_link,
+                                                      npoints,
                                                       angle_increment,
-                                                      radius_increment,
-                                                      radius_max,
+                                                      radius_x_max,
+                                                      radius_y_max,
                                                       speed, accel, timeout),
                                      done_cb, active_cb)
+
+    def notify_success(self):
+        self._success = True
+        self.cancel_goal()
 
     # Server stuffs
     def shutdown(self):
         self._server.__del__()
 
     def _execute_cb(self, goal):
-        rospy.loginfo("*** Do spiral_searching ***")
+        rospy.loginfo('(spiral_search) goal ACCEPTED')
         eef_link = goal.eef_link
         if eef_link == '':
             eef_link = self._routines.gripper(goal.robot_name).tip_link
         waypoints = self._create_waypoints(eef_link,
+                                           goal.npoints,
                                            goal.angle_increment,
-                                           goal.radius_increment,
-                                           goal.radius_max)
+                                           goal.radius_x_max,
+                                           goal.radius_y_max)
         timeout_time = rospy.get_rostime() + goal.timeout
-        while rospy.get_rostime() < timeout_time:
+        while goal.timeout <= rospy.Duration() or \
+              rospy.get_rostime() < timeout_time:
             if not self._server.is_active():
                 return
             self._routines.go_along_poses(goal.robot_name, waypoints,
                                           speed=goal.speed, accel=goal.accel,
                                           end_effector_link=eef_link)
-        self._server.set_succeeded()
+        self._server.set_aborted()
+        rospy.logerr('(spiral_search) goal ABORTED: timeout[%f]',
+                     goal.timeout_time.to_sec())
 
     def _preempt_cb(self):
         goal = self._server.current_goal.get_goal()
         self._routines.stop(goal.robot_name)
-        self._server.set_preempted()
-        rospy.logwarn('--- SpiralSearch cancelled. ---')
+        if self._success:
+            self._server.set_succeeded()
+            rospy.logwarn('(spiral_search) goal SUCCEEDED')
+        else:
+            self._server.set_preempted()
+            rospy.logwarn('(spiral_search) goal CANCELLED')
 
-    def _create_waypoints(self, eef_link,
-                          angle_increment, radius_increment, radius_max):
+    def _create_waypoints(self, eef_link, npoints, angle_increment,
+                          radius_x_max, radius_y_max):
         poses = PoseArray()
         poses.header.frame_id = eef_link
+
         a = 0.0
-        for r in np.arange(0.0, radius_max, radius_increment):
-            poses.poses.append(Pose(Point(r*np.cos(a), r*np.sin(a), 0),
+        for n in range(0, npoints + 1):
+            rx = n*radius_x_max/npoints
+            ry = n*radius_y_max/npoints
+            poses.poses.append(Pose(Point(rx*np.cos(a), ry*np.sin(a), 0),
                                     Quaternion(0, 0, 0, 1)))
             a += np.radians(angle_increment)
         poses.poses.extend(list(reversed(copy.deepcopy(poses.poses[1:-1]))))
