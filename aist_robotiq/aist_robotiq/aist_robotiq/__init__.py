4e888f9a17c3d91363af71ba5e4f3806bb12e701
t-ueshiba <t.ueshiba@aist.go.jp>
2025-06-20 06:24:40 +0900

    WIP

--- aist_robotiq/aist_robotiq/aist_robotiq/__init__.py
+++ aist_robotiq/aist_robotiq/aist_robotiq/__init__.py
@@ -36,12 +36,12 @@ Clients of gripper action controller of control_msg/GripperCommandAction type.
 @author t.ueshiba@aist.go.jp
 """
 import rclpy
-from rclpy.node   import Node
-from rclpy.action import ActionClient
-from actionlib_msgs.msg import GoalStatus
-from control_msgs.action import GripperCommand
-from control_msgs.msg    import GripperCommand as GripperCommandMsg
-from aist_robotiq.action import EPickCommand
+from rclpy.node               import Node
+from rclpy.action             import ActionClient
+from actionlib_msgs.msg       import GoalStatus
+from control_msgs.action      import GripperCommand
+from control_msgs.msg         import GripperCommand as GripperCommandMsg
+from aist_robotiq_msgs.action import EPickCommand
 
 ######################################################################
 #  class GenericGripper                                              #
@@ -69,7 +69,7 @@ class GenericGripper(Node):
                             'release_position': max_position,
                             'max_effort':       max_effort}
 
-        rospy.get_loger().info('%s initialized.', action_ns)
+        self.get_loger().info('%s initialized.', action_ns)
 
     @property
     def parameters(self):
@@ -88,7 +88,7 @@ class GenericGripper(Node):
         for key, value in parameters.items():
             self._parameters[key] = value
 
-    def grasp(self, timeout=rospy.Duration(0)):
+    def grasp(self, timeout=rclpy.duration(0)):
         """
         Grasp an object with the gripper.
         Desired finger position and applied effort are specified by parameters
@@ -103,7 +103,7 @@ class GenericGripper(Node):
         return self.move(self.parameters['grasp_position'],
                          self.parameters['max_effort'], timeout)
 
-    def release(self, timeout=rospy.Duration(0)):
+    def release(self, timeout=rclpy.duration(0)):
         """
         Release an object grasped by the gripper.
         Desired finger position is specified by a parameter
@@ -117,7 +117,7 @@ class GenericGripper(Node):
         """
         return self.move(self.parameters['release_position'], 0, timeout)
 
-    def move(self, position, max_effort=0, timeout=rospy.Duration(0)):
+    def move(self, position, max_effort=0, timeout=rclpy.duration(0)):
         """
         Move fingers to the specified position with specified effort
         @param position   finger position
@@ -135,7 +135,7 @@ class GenericGripper(Node):
                                      feedback_callback=self._feedback_cb)
         return self.wait(timeout)
 
-    def wait(self, timeout=rospy.Duration(0)):
+    def wait(self, timeout=rclpy.duration(0)):
         """
         Wait the gripper for completing the movement.
         @param timeout If positive, wait timeout duration until
@@ -148,12 +148,12 @@ class GenericGripper(Node):
         if timeout.to_sec() < 0:
             return GripperCommandResult(0, 0, False, False)
         elif not self._client.wait_for_result(timeout):
-            rospy.logerr('Timeout[%f] has expired before goal finished',
-                         timeout.to_sec())
-            return GripperCommandResult(self._feedback.position,
-                                        self._feedback.effort,
-                                        self._feedback.stalled,
-                                        self._feedback.reached_goal)
+            self.get_logger().error('Timeout[%f] has expired before goal finished',
+                                    timeout.to_sec())
+            return GripperCommand.Result(self._feedback.position,
+                                         self._feedback.effort,
+                                         self._feedback.stalled,
+                                         self._feedback.reached_goal)
         return self._client.get_result()
 
     def cancel(self):
@@ -178,10 +178,10 @@ class RobotiqGripper(GenericGripper):
         @param max_effort maximum effort applied when gripping objects
         """
         ns = prefix + 'controller'
-        self._min_gap      = rospy.get_param(ns + '/min_gap',      0.000)
-        self._max_gap      = rospy.get_param(ns + '/max_gap',      0.085)
-        self._min_position = rospy.get_param(ns + '/min_position', 0.81)
-        self._max_position = rospy.get_param(ns + '/max_position', 0.00)
+        self._min_gap      = rclpy.get_param(ns + '/min_gap',      0.000)
+        self._max_gap      = rclpy.get_param(ns + '/max_gap',      0.085)
+        self._min_position = rclpy.get_param(ns + '/min_position', 0.81)
+        self._max_position = rclpy.get_param(ns + '/max_position', 0.00)
 
         assert self._min_gap < self._max_gap
         assert self._min_position != self._max_position
@@ -189,10 +189,10 @@ class RobotiqGripper(GenericGripper):
         super().__init__(ns + '/gripper_cmd',
                          self._min_gap, self._max_gap, max_effort)
 
-    def move(self, gap, max_effort=0, timeout=rospy.Duration(0)):
+    def move(self, gap, max_effort=0, timeout=rclpy.duration(0)):
         return super().move(self._position(gap), max_effort, timeout)
 
-    def wait(self, timeout=rospy.Duration(0)):
+    def wait(self, timeout=rclpy.duration(0)):
         result = super().wait(timeout)
         result.position = self._gap(result.position)
         return result
@@ -238,7 +238,7 @@ class EPickGripper(Node):
                             'detection_pressure': detection_pressure,
                             'release_pressure':   release_pressure}
 
-        rospy.loginfo('%s initialized.', ns + '/gripper_cmd')
+        self.get_logger().info('%s initialized.', ns + '/gripper_cmd')
 
     @property
     def parameters(self):
@@ -257,7 +257,7 @@ class EPickGripper(Node):
         for key, value in parameters.items():
             self._parameters[key] = value
 
-    def grasp(self, timeout=rospy.Duration(0)):
+    def grasp(self, timeout=rclpy.duration(0)):
         """
         Grasp an object with the gripper.
         Pressure applied and pressure threshold for object detection are
@@ -274,7 +274,7 @@ class EPickGripper(Node):
                          self.parameters['detection_pressure'],
                          timeout)
 
-    def release(self, timeout=rospy.Duration(-1)):
+    def release(self, timeout=rclpy.duration(-1)):
         """
         Release an object grasped by the gripper.
         Value of applied pressure is specified by a parameter
@@ -290,7 +290,7 @@ class EPickGripper(Node):
                          self.parameters['detection_pressure'],
                          timeout)
 
-    def move(self, max_pressure, min_pressure, timeout=rospy.Duration(0)):
+    def move(self, max_pressure, min_pressure, timeout=rclpy.duration(0)):
         """
         Move fingers to the specified position with specified effort
         @param max_pressure maximum pressure value applied
@@ -302,14 +302,14 @@ class EPickGripper(Node):
                             for completion.
         @return result of aist_robotiq/EPickCommandResult type
         """
-        self._client.send_goal(EPickCommandGoal(
+        self._client.send_goal(EPickCommand.Goal(
                                  EPickCommand(self.parameters['advanced_mode'],
                                               max_pressure, min_pressure,
                                               timeout)),
                                feedback_cb=self._feedback_cb)
         return self.wait(timeout)
 
-    def wait(self, timeout=rospy.Duration(0)):
+    def wait(self, timeout=rclpy.duration(0)):
         """
         Wait the gripper for completing the movement.
         @param timeout If positive, wait timeout duration until
@@ -322,9 +322,9 @@ class EPickGripper(Node):
         if timeout.to_sec() < 0:
             return EPickCommandResult(0, False)
         elif not self._client.wait_for_result(timeout):
-            rospy.logerr('Timeout[%f] has expired before goal finished',
-                         timeout.to_sec())
-            return EPickCommandResult(self._feedback.pressure,
+            self.get_logger().error('Timeout[%f] has expired before goal finished',
+                                    timeout.to_sec())
+            return EPickCommand.Result(self._feedback.pressure,
                                       self._feedback.stalled)
         return self._client.get_result()
 
