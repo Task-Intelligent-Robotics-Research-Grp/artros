cfbd0647e32204d6c90c810e5f11ce05eddca387
t-ueshiba <t.ueshiba@aist.go.jp>
2026-04-21 08:15:12 +0900

    WIP: Introduce payload builder/decoder into CModelModbus driver

--- aist_robotiq/aist_robotiq/aist_robotiq/cmodel_modbus.py
+++ aist_robotiq/aist_robotiq/aist_robotiq/cmodel_modbus.py
@@ -38,6 +38,8 @@ from aist_robotiq.cmodel_base import CModelBase
 from aist_robotiq_msgs.msg    import CModelStatus
 from pymodbus.exceptions      import ModbusIOException
 from pymodbus.client          import ModbusTcpClient, ModbusSerialClient
+from pymodbus.payload         import BinaryPayloadBuilder, BinaryPayloadDecoder
+from pymodbus.constants       import Endian
 
 #########################################################################
 #  class CModelModbusBase                                               #
@@ -54,85 +56,77 @@ class CModelModbusBase(CModelBase):
         # Clip each field of command within a valid range.
         command = self._clip_command(command)
 
-        # Convert the command to a byte array of 6-length.
-        data = []
-        data.append(command.r_act |
-                    (command.r_mod << 1) | (command.r_gto << 3) |
-                    (command.r_atr << 4) | (command.r_ard << 5))  # Byte 0
-        data.append((command.r_icf << 2) | (command.r_ics << 3))  # Byte 1
-        data.append(0)                                            # Byte 2
-        data.append(command.r_pr)                                 # Byte 3
-        data.append(command.r_sp)                                 # Byte 4
-        data.append(command.r_fr)                                 # Byte 5
+        # Convert the command to a byte array of 6/15-length.
+        builder = BinaryPayloadBuilder(byteorder=Endian.BIG,
+                                       wordorder=Endian.BIG)
+        builder.add_8bit_uint(command.r_act |
+                              (command.r_mod << 1) |
+                              (command.r_gto << 3) |
+                              (command.r_atr << 4) |
+                              (command.r_ard << 5))     # Byte 0
+        builder.add_8bit_uint((command.r_icf << 2) |
+                              (command.r_ics << 3))     # Byte 1
+        builder.add_8bit_uint(0)                        # Byte 2
+        builder.add_8bit_uint(command.r_pr)             # Byte 3
+        builder.add_8bit_uint(command.r_sp)             # Byte 4
+        builder.add_8bit_uint(command.r_fr)             # Byte 5
         if self._arg3f[command.r_sid]:
-            data.append(command.r_prb)                            # Byte 6
-            data.append(command.r_spb)                            # Byte 7
-            data.append(command.r_frb)                            # Byte 8
-            data.append(command.r_prc)                            # Byte 9
-            data.append(command.r_spc)                            # Byte 10
-            data.append(command.r_frc)                            # Byte 11
-            data.append(command.r_prs)                            # Byte 12
-            data.append(command.r_sps)                            # Byte 13
-            data.append(command.r_frs)                            # Byte 14
-        self._put_command(data, command.r_sid)
+            builder.add_8bit_uint(command.r_prb)        # Byte 6
+            builder.add_8bit_uint(command.r_spb)        # Byte 7
+            builder.add_8bit_uint(command.r_frb)        # Byte 8
+            builder.add_8bit_uint(command.r_prc)        # Byte 9
+            builder.add_8bit_uint(command.r_spc)        # Byte 10
+            builder.add_8bit_uint(command.r_frc)        # Byte 11
+            builder.add_8bit_uint(command.r_prs)        # Byte 12
+            builder.add_8bit_uint(command.r_sps)        # Byte 13
+            builder.add_8bit_uint(command.r_frs)        # Byte 14
+        self._write_registers(builder.to_registers(), command.r_sid)
 
     def get_status(self, slave_id):
         # Acquire status from the Gripper
-        data = self._get_status(15 if self._arg3f[slave_id] else 6, slave_id)
+        nregs = 8 if self._arg3f[slave_id] else 3
+        try:
+            result = self._read_registers(nregs, slave_id)
+            if result.isError():
+                raise RuntimeError(result)
+        except Exception as e:
+            self.get_logger().error(f'Error: {e}')
+            return
+
+        decoder = BinaryPayloadDecoder.fromRegisters(result.registers,
+                                                     byteorder=Endian.BIG,
+                                                     wordorder=Endian.BIG)
 
         # Assign the values to their respective variables
         status = CModelStatus()
         status.g_sid = slave_id
-        status.g_act =  data[0]       & 0x01    # Byte 0
-        status.g_mod = (data[0] >> 1) & 0x03
-        status.g_gto = (data[0] >> 3) & 0x01
-        status.g_sta = (data[0] >> 4) & 0x03
-        status.g_obj = (data[0] >> 6) & 0x03
-        status.g_vas =  data[1]       & 0x03    # Byte 1
-        status.g_dtb = (data[1] >> 2) & 0x03
-        status.g_dtc = (data[1] >> 4) & 0x03
-        status.g_dts = (data[1] >> 6) & 0x03
-        status.g_flt =  data[2]       & 0x0f    # Byte 2
-        status.g_pr  =  data[3]                 # Byte 3
-        status.g_po  =  data[4]                 # Byte 4
-        status.g_cou =  data[5]                 # Byte 5
+        data = decoder.decode_8bit_uint()               # Byte 0
+        status.g_act =  data       & 0x01
+        status.g_mod = (data >> 1) & 0x03
+        status.g_gto = (data >> 3) & 0x01
+        status.g_sta = (data >> 4) & 0x03
+        status.g_obj = (data >> 6) & 0x03
+        data = decorer.decode_8bit_uint()               # Byte 1
+        status.g_vas =  data       & 0x03
+        status.g_dtb = (data >> 2) & 0x03
+        status.g_dtc = (data >> 4) & 0x03
+        status.g_dts = (data >> 6) & 0x03
+        data = decorer.decode_8bit_uint()               # Byte 2
+        status.g_flt = data        & 0x0f
+        status.g_pr  = decorer.decode_8bit_uint()       # Byte 3
+        status.g_po  = decorer.decode_8bit_uint()       # Byte 4
+        status.g_cou = decorer.decode_8bit_uint()       # Byte 5
         if self._arg3f[slave_id]:
-            status.g_prb = data[6]              # Byte 6
-            status.g_pob = data[7]              # Byte 7
-            status.g_cub = data[8]              # Byte 8
-            status.g_prc = data[9]              # Byte 9
-            status.g_poc = data[10]             # Byte 10
-            status.g_cuc = data[11]             # Byte 11
-            status.g_prs = data[12]             # Byte 12
-            status.g_pos = data[13]             # Byte 13
-            status.g_cus = data[14]             # Byte 14
+            status.g_prb = decorer.decode_8bit_uint()   # Byte 6
+            status.g_pob = decorer.decode_8bit_uint()   # Byte 7
+            status.g_cub = decorer.decode_8bit_uint()   # Byte 8
+            status.g_prc = decorer.decode_8bit_uint()   # Byte 9
+            status.g_poc = decorer.decode_8bit_uint()   # Byte 10
+            status.g_cuc = decorer.decode_8bit_uint()   # Byte 11
+            status.g_prs = decorer.decode_8bit_uint()   # Byte 12
+            status.g_pos = decorer.decode_8bit_uint()   # Byte 13
+            status.g_cus = decorer.decode_8bit_uint()   # Byte 14
         return status
-
-    def _put_command(self, data, slave_id):
-        # Make sure data has an even number of elements
-        if len(data) % 2 == 1:
-            data.append(0)
-
-        # Compose every two datas into one register word in big-endian order.
-        message = []
-        for i in range(0, len(data), 2):
-            message.append((data[i] << 8) | data[i+1])
-        self._write_registers(message, slave_id)  # (defined in derived class)
-
-    def _get_status(self, nbytes, slave_id):
-        nregs    = 2*((nbytes - 1)//2)
-        response = self._read_registers(nregs,
-                                        slave_id) # (defined in derived class)
-        if response.isError():
-            self.get_logger().error(str(response))
-            raise RuntimeError(response)
-
-        # Decompose each register word to two bytes in little-endian order.
-        data = []
-        for val in response.registers:
-            data.append((val & 0xFF00) >> 8)
-            data.append( val & 0x00FF)
-        return data
 
 #########################################################################
 #  class CModelModbusTCP                                                #
@@ -148,9 +142,9 @@ class CModelModbusTCP(CModelModbusBase):
             raise
         self.get_logger().info('started[ip=%s]' % ip)
 
-    def _write_registers(self, message, slave_id):
+    def _write_registers(self, registers, slave_id):
         with self._lock:
-            self._client.write_registers(0, message, slave_id)
+            self._client.write_registers(0, registers, slave_id)
 
     def _read_registers(self, nregs, slave_id):
         with self._lock:
@@ -172,9 +166,9 @@ class CModelModbusRTU(CModelModbusBase):
             raise
         self.get_logger().info('started[dev=%s]' % dev)
 
-    def _write_registers(self, message, slave_id):
+    def _write_registers(self, registers, slave_id):
         with self._lock:
-            self._client.write_registers(0x03E8, message, slave_id)
+            self._client.write_registers(0x03E8, registers, slave_id)
 
     def _read_registers(self, nregs, slave_id):
         with self._lock:
