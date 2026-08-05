27feea84b88f7edf42bedade91801d0264757a69
Toshio UESHIBA <t.ueshiba@aist.go.jp>
2026-08-05 18:39:01 +0900

    WIP

--- aist_fastening_tools/aist_fastening_tools/client.py
+++ aist_fastening_tools/aist_fastening_tools/client.py
@@ -50,7 +50,7 @@ class SuctionTool(SimpleActionClient):
     """Suction tool client of aist_msgs.action.SuctionToolCommand type.
     """
     def __init__(self, node, name, *,
-                 suck_min_period=2.0, blow_min_period=2.0, grasp_timeout=5.0):
+                 suck_min_period=0.5, blow_min_period=0.2, grasp_timeout=5.0):
         """Create SuctionTool
 
         :param node: The ROS node to add the suction tool client to.
@@ -107,7 +107,7 @@ class SuctionTool(SimpleActionClient):
         # Set goal.min_period to zero so that the goal succeeds immediately.
         self._suck_command(True, min_period=0.0, timeout_sec=0.0)
 
-    def grasp(self, *, timeout_sec=None):
+    def grasp(self, *, timeout_sec=0.0):
         return self._suck_command(
                    True, min_period=self._parameters['suck_min_period'],
                    timeout_sec=timeout_sec)
@@ -148,10 +148,10 @@ class SuctionTool(SimpleActionClient):
         return self._suctioned
 
     def _suck_command(self, suck, *, min_period, timeout_sec=None):
-        if timeout_sec > 0.0:
-            grasp_timeout = timeout_sec  # Wait same duration as action timeout
-        else:
+        if timeout_sec is None or timeout_sec <= 0.0:
             grasp_timeout = self.parameters['grasp_timeout']
+        else:
+            grasp_timeout = timeout_sec  # Wait same duration as action timeout
         return self.send_goal(SuctionToolCommand.Goal(suck=suck,
                                                       min_period=min_period,
                                                       timeout=grasp_timeout),
